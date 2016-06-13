52544882ddcec40cb5a4beb9054e973750955ead
Toby Thomson <tobythomson1@yahoo.co.uk>
2016-06-13 17:20:39 +0100

    Tornado serves static content

--- TornadoServer.py
+++ TornadoServer.py
@@ -3,23 +3,31 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+import os
 
 
-class UploadHandler(tornado.web.RequestHandler):
+class UploadHandler (tornado.web.RequestHandler):
 	def post(self):
 		print('PRINT FILE RECIEVED\n')
 
-class IndexPageHandler(tornado.web.RequestHandler):
+class MainHandler (tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
 application = tornado.web.Application([
-	(r'/', IndexPageHandler),
+	(r'/', MainHandler),
 	(r'/upload/', UploadHandler)
 
-], debug=True)
+],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        debug=True
+)
 
 if __name__ == "__main__":
 	http_server = tornado.httpserver.HTTPServer(application)
 	http_server.listen(8888)
 	tornado.ioloop.IOLoop.instance().start()
+
+
+
